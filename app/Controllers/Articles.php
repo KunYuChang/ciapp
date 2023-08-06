4a7218b00949a699fd1b17cd1bbc8e45f4b2d17c
KunYuChang <mischiefsub@gmail.com>
2023-08-06 15:29:35 +0800

    Models: Connet to the Database and Select Data Automatically

--- app/Controllers/Articles.php
+++ app/Controllers/Articles.php
@@ -1,16 +1,14 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\ArticleModel;
 
 class Articles extends BaseController
 {
     public function index()
     {
-        // 目前先從 controller 產生資料，送到 view
-        $data = [
-            ["title" => "One", "content" => "The first"],
-            ["title" => "Two", "content" => "Some content"],
-        ];
+        $model = new ArticleModel;
+        $data = $model->findAll(); // this method is inherited from the base model
 
         return view("Articles/index", [
             "articles" => $data
