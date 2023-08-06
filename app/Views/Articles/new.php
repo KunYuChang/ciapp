9f773d9a8575777379f8a89ce23d590f4ecf7b51
KunYuChang <mischiefsub@gmail.com>
2023-08-06 19:14:31 +0800

    Add a form with input elements

--- app/Views/Articles/new.php
+++ app/Views/Articles/new.php
@@ -8,4 +8,13 @@
 
 <h1>New Article</h1>
 
+<!-- Form -->
+<?= form_open("articles/create") ?>
+<label for="title">Title</label>
+<input type="text" id="title" name="title">
+<label for="content">Content</label>
+<input type="text" id="content" name="content">
+<button>Save</button>
+<?= form_close() ?>
+
 <?= $this->endSection() ?>
