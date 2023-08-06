04893402e4223a9d7240705b15dfb9b9d35cf5d9
KunYuChang <mischiefsub@gmail.com>
2023-08-06 11:59:39 +0800

    display dynamic data in a view

--- app/Views/Articles/index.php
+++ app/Views/Articles/index.php
@@ -6,4 +6,12 @@
 <!-- Content -->
 <?= $this->section("content") ?>
     <h1>Articles</h1>
+
+    <?php foreach ($articles as $article): ?> 
+        <article>
+            <h2><?= $article["title"]?></h2>
+            <p><?= $article["content"]?></p>
+        </article>
+    <?php endforeach; ?>
+
 <?= $this->endSection() ?>
